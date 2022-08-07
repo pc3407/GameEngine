9261dd79f206b73490078e147bce8c0902311784
pc3407 <pc3407@naver.com>
2022-08-07 14:47:35 +0900

    Create Main.cpp
    Win32 Entry Point

---
+++ Client/Main.cpp
@@ -0,0 +1,9 @@
+#include <windows.h>
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance,
+                   _In_opt_ HINSTANCE hPrevInstance,
+                   _In_ LPSTR lpCmdLine,
+                   _In_ int nCmdShow)
+{
+    return 0;
+}
