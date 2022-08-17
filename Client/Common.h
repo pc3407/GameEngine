ea5899183f105cf4e235d714c34c6ba3857be868
pc3407 <pc3407@naver.com>
2022-08-17 21:16:00 +0900

    Update Common.h

--- Client/Common.h
+++ Client/Common.h
@@ -1,18 +1,20 @@
 #pragma once
 
 #include <assert.h>
+#include <windows.h>
+#include <crtdbg.h>
 
 #include <d3d11.h>
 #pragma comment(lib, "d3d11.lib")
+
+#include <dxgi1_2.h>
+#pragma comment(lib, "dxgi.lib")
 
 #include <d3dcompiler.h>
 #pragma comment(lib,"d3dcompiler.lib")
 
 #include <DirectXMath.h>
 using namespace DirectX;
-
-#include "Singleton.h"
-#include "Core.h"
 
 #define SAFE_RELEASE(p)     { if(p) { (p)->Release(); (p) = NULL; } }
 #define SAFE_DELETE(p)      { if(p) { delete (p); (p) = NULL; } }
