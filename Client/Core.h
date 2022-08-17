fafa591be7304ba6e6124ec486ffbbaeb0452984
pc3407 <pc3407@naver.com>
2022-08-17 21:15:56 +0900

    Core modity

--- Client/Core.h
+++ Client/Core.h
@@ -1,14 +1,6 @@
 #pragma once
-#include <assert.h>
-
-#include <d3d11.h>
-#pragma comment(lib, "d3d11.lib")
-
-#define SAFE_RELEASE(p)     { if(p) { (p)->Release(); (p) = NULL; } }
-#define SAFE_DELETE(p)      { if(p) { delete (p); (p) = NULL; } }
-#define SAFE_DELETE_ARRAR(p){ if(p) { delete [] (p); (p) = NULL; } }
-
 #include "Singleton.h"
+
 class Core : public Singleton<Core>
 {
     friend class Singleton<Core>;
@@ -20,12 +12,4 @@ private:
 public:
     void init(HWND hWnd, int width = 1280, int height = 720);
     void progress();
-
-private:
-    IDXGISwapChain          *pSwapChain;
-    ID3D11Device            *pDevice;
-    ID3D11DeviceContext     *pImmediateContext;
-
-    ID3D11RenderTargetView  *pRenderTargetView;
-    ID3D11DepthStencilView  *pDepthStencilView;
 };
