63c615ab199c7f43842854e259924a66a70185ca
pc3407 <pc3407@naver.com>
2022-09-06 19:23:22 +0900

    Create Math.h and cpp

---
+++ Client/Math.cpp
@@ -0,0 +1,5 @@
+#include "Math.h"
+
+const float Math::Deg2Rad = (Math::PI * 2) / 360;
+const float Math::PI = 3.14159265358979f;
+const float Math::Rad2Deg = 360 / (Math::PI * 2);
---
+++ Client/Math.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class Math
+{
+public:
+	static const float Deg2Rad;
+	static const float PI;
+	static const float Rad2Deg;
+};
